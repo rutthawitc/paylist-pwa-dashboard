e97403353673b68ffb668d24cf5a4fbd0f031485
rutthawit ch <rutthawit@gmail.com>
2024-07-11 11:17:13 +0700

    -Fix Mobile-Menu-List.tsx#2

--- components/protected/mobile-menu-list.tsx
+++ components/protected/mobile-menu-list.tsx
@@ -20,7 +20,7 @@ const MobileMenuList = () => {
         <span className='sr-only'>PWA</span>
       </Link>
       <Link
-        href='#'
+        href='/'
         className='mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground'>
         <Home className='h-5 w-5' />
         Dashboard
