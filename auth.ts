276b8e217bcac150b7cab433b243323dba9921e9
rutthawit ch <rutthawit@gmail.com>
2025-02-21 11:56:41 +0700

    ♻️ (auth.ts): update session maxAge from 1 hour to 15 minutes for improved security The session maxAge is reduced to 15 minutes to enhance security by limiting the duration of user sessions. This change helps mitigate risks associated with long-lived sessions, ensuring that users are required to re-authenticate more frequently.

--- auth.ts
+++ auth.ts
@@ -46,7 +46,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   session: {
     strategy: 'jwt',
-    maxAge: 60 * 60, // 1 ชั่วโมง
+    maxAge: 15 * 60, // 15 นาที
   },
   secret: process.env.NEXTAUTH_SECRET
 });
