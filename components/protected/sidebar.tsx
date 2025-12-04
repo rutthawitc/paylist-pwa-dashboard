203432dcd1b166448c131977466692f877fef4b0
Rutthawit Chuennuawapan <rutthawitc@MacBook-Air-khxng-Rutthawit.local>
2025-12-04 10:00:42 +0700

    fix: redirect to root after logout

--- components/protected/sidebar.tsx
+++ components/protected/sidebar.tsx
@@ -15,7 +15,7 @@ import { signOut } from 'next-auth/react';
 
 const Sidebar = () => {
   const handleSignOut = async () => {
-    await signOut({ callbackUrl: '/dashboard' });
+    await signOut({ callbackUrl: '/' });
   };
   return (
     <div className='hidden border-r bg-muted/40 md:block'>
